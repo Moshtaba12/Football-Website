8e82afceba7d38fe23e3853a3a72ad5bef550fbb
Moshtaba <m.mustan12@gmail.com>
2021-08-06 23:17:10 +0100

    added hover js effects and improved animations

---
+++ app.js
@@ -0,0 +1,24 @@
+var mini = true;
+
+function toggleSidebar() {
+  if (mini == true){
+    // console.log("inside the side bar");
+    document.getElementById("mySidebar").style.width = "240px";
+    document.getElementById("navBox_1").style.width = "200px"
+    document.getElementById("navBox_2").style.width = "200px"
+    document.getElementById("navBox_3").style.width = "200px"
+    document.getElementById("navBox_4").style.width = "200px"
+    document.getElementById("navBox_5").style.width = "200px"
+    this.mini = false;
+  }
+  else{
+    // console.log("Closing sidebar");
+    document.getElementById("mySidebar").style.width = "78px";
+    document.getElementById("navBox_1").style.width = "48px"
+    document.getElementById("navBox_2").style.width = "48px"
+    document.getElementById("navBox_3").style.width = "48px"
+    document.getElementById("navBox_4").style.width = "48px"
+    document.getElementById("navBox_5").style.width = "48px"
+    this.mini = true;
+  }
+}
